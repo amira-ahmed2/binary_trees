91a3590b3e47bf098cc2b42fd6fd6a24ff642492
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-01-30 18:25:50 +0200

    Create 4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - that checks if a node is a leaf
+ * @node: pointer to the node to check
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node == NULL || node->left != NULL || node->right != NULL)
+return (0);
+return (1);
+}
