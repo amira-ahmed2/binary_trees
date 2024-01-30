4eadc291c385f52581459d00b3f8e6e0b2939acb
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-01-30 17:19:55 +0200

    add

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Stores recursively each level in an array of strings
+ *
+ * @value: Pointer to the node to print
+ * Return: length of printed tree after process
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+if (new_node == NULL)
+	return NULL;
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return new_node;
+}
