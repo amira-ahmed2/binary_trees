c181126d1603895666c06f8d530ebdf5d02dd976
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-01-30 19:40:37 +0200

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t l = 0, r = 0;
+		size_t left = 0;
+		size_t right = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((l > r) ? l : r);
+		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((left > right) ? left : right);
 	}
 	return (0);
 }
