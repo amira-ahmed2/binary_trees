b87c1f0e7fa1ef83369190ba14bbd04c0f939327
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-01-30 16:05:57 +0000

    update

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,15 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+fndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+
+/* basic binary tree */
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,21 +19,29 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-int n;
-struct binary_tree_s *parent;
-struct binary_tree_s *left;
-struct binary_tree_s *right;
-} binary_tree_t;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+};
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
 typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+size_t _binary_tree_height(const binary_tree_t *tree);
+
 #endif
