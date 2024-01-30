6b1428876b6429cc03aa6a829711e0c5e649ccf7
amira-ahmed2 <amiraa.amin300@yahoo.com>
2024-01-30 17:59:48 +0200

    update

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child
+ * @parent: pointer to the node to insert the left-child in
+ * @value: Pointer to the node to print
+ * Return: length of printed tree after process
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+binary_tree_t *newNode;
+
+if (parent == NULL)
+return (NULL);
+
+newNode = binary_tree_node(parent, value);
+if (newNode == NULL)
+return (NULL);
+
+if (parent->right != NULL)
+{
+newNode->right = parent->right;
+parent->right->parent = newNode;
+}
+parent->right = newNode;
+
+return (newNode);
+}
