7ce64fa4001c135641c12783c1dd593596731aca
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-01-30 19:20:16 +0200

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,10 +7,10 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree && fun)
-  {
-    func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
-  }
+if (tree && fun)
+{
+func(tree->n);
+binary_tree_preorder(tree->left, func);
+binary_tree_preorder(tree->right, func);
+}
 }
