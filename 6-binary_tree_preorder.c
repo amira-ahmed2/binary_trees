7151e80bd46eeaa4ec535add9bfee9f27a396dc4
AmiraAhmed <106966309+amira-ahmed2@users.noreply.github.com>
2024-01-30 19:19:24 +0200

    Create 6-binary_tree_preorder.c

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - that checks if a given node is a root
+ * @tree: pointer to the root node of the tree to traverse
+ * @func: pointer to a function to call for each node.
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && fun)
+  {
+    func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+  }
+}
